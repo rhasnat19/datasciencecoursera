f2391a7d7315675da743add069a4a6c43155ffdb
rhasnat19 <57846249+rhasnat19@users.noreply.github.com>
2020-06-28 17:05:11 -0700

    Update Assignment1.R

--- Assignment1.R
+++ Assignment1.R
@@ -1,54 +1,24 @@
-## The following function create "matrix" object that will cache its inverse.
-
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
+  set<-function(y){
+    x<<-y
+    inv <<-NULL
   }
-  get <- function() x
-  setInverse <- function(inverse) inv <<- inverse
-  getInverse <- function() inv
-  list(set = set,
-       get = get,
-       setInverse = setInverse,
-       getInverse = getInverse)
+  get <-function(){x}
+  setinverse <-function(Minverse){inv<<-Minverse}
+  getinverse <- function(){inv}
+  list(set=set, get=get, setinverse=setinverse,getinverse=getinverse)
 }
-## This function computes the inverse of the "matrix" makeCacheMatrix. 
-##If the inverse has been previously calculated and the matrix has not been changed, the inverse will be retrieved from the cache.
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  inv <- x$getInverse()
-  if (!is.null(inv)) {
-    message("getting cached data")
+  inv<- x$getinverse()
+  if(!is.null(inv)){
+    message("data in memory")
     return(inv)
   }
   mat <- x$get()
   inv <- solve(mat, ...)
-  x$setInverse(inv)
+  x$setinverse(inv)
   inv
 }
-## First we can create a "matrix" with the numbers from 1 to 4 and create cache and retrieve the inverse
 
-my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
-my_matrix$get()
-cacheSolve(my_matrix)
-cacheSolve(my_matrix)
-my_matrix$getInverse()
-my_matrix$getInverse()
-
-## We can then take any random numbers and create cache and retrieve the inverse
-## We will two different "matrices" with randomly chosen numbers
-
-my_matrix <- makeCacheMatrix(matrix(c(105,95,766,83), 2, 2))
-my_matrix$get()
-cacheSolve(my_matrix)
-cacheSolve(my_matrix)
-my_matrix$getInverse()
-
-my_matrix <- makeCacheMatrix(matrix(c(657, 6373, 546, 3), 2, 2))
-my_matrix$get()
-cacheSolve(my_matrix)
-cacheSolve(my_matrix)
-my_matrix$getInverse()
